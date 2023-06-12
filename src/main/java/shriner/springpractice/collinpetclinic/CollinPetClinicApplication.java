3969c439bd0eb508a2df5a951af24e621aa2c52f
Collin Shriner <collinshriner9@gmail.com>
2023-06-12 16:57:49 -0400

    Initial Project Add

---
+++ src/main/java/shriner/springpractice/collinpetclinic/CollinPetClinicApplication.java
@@ -0,0 +1,13 @@
+package shriner.springpractice.collinpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollinPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollinPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shriner/springpractice/collinpetclinic/CollinPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package shriner.springpractice.collinpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollinPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
