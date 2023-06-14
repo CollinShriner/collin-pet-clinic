1755aa598f4b7ec532f83b9c9d7e23aaf9ff57ba
Collin Shriner <collinshriner9@gmail.com>
2023-06-14 15:22:47 -0400

    AbstractMapService now contains a method that gets the next id value available. This is used to generate the id when a new entity is saved. close #22

--- pet-clinic-web/src/main/java/shriner/springpractice/collinpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/shriner/springpractice/collinpetclinic/bootstrap/DataLoader.java
@@ -24,14 +24,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Collin");
         owner1.setLastName("Shriner");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Steve");
         owner2.setLastName("Mine");
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("(Vet) First");
         vet1.setLastName("(Vet) Last");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("(Vet) Unique");
         vet2.setLastName("(Vet) Name");
 
