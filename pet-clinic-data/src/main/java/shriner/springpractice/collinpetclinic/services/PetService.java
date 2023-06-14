35853d94dd515f6cc06a631bb284ccaff83a2040
Collin Shriner <collinshriner9@gmail.com>
2023-06-14 09:49:28 -0400

    refactored into common crud interface

--- pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package shriner.springpractice.collinpetclinic.services;
 
 import shriner.springpractice.collinpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package shriner.springpractice.collinpetclinic.services;
 
 import shriner.springpractice.collinpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
