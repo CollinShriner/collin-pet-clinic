9bf5fabdf2b446d5ded28544915c2ddf143e476c
Collin Shriner <collinshriner9@gmail.com>
2023-06-16 09:29:15 -0400

    created pet type map service

---
+++ pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package shriner.springpractice.collinpetclinic.services;
+
+import shriner.springpractice.collinpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package shriner.springpractice.collinpetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import shriner.springpractice.collinpetclinic.model.PetType;
+import shriner.springpractice.collinpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
