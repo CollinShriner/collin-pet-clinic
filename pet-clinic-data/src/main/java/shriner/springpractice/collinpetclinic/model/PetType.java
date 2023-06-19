508cd841ae839920e40e98ca908f0d1797984c27
Collin Shriner <collinshriner9@gmail.com>
2023-06-19 14:01:47 -0400

    Updated owner with JPA. Close #34

--- pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/model/PetType.java
@@ -1,6 +1,11 @@
 package shriner.springpractice.collinpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity(name = "types")
 public class PetType extends BaseEntity{
+    @Column(name = "name")
     private String name;
 
     public String getName() {
