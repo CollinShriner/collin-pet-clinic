b4bcd7f77c1899586394b9a5c1ad60bdbbcc1cd7
Collin Shriner <collinshriner9@gmail.com>
2023-06-21 15:48:08 -0400

    added project lombok close #49

--- pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/shriner/springpractice/collinpetclinic/model/PetType.java
@@ -2,17 +2,16 @@ package shriner.springpractice.collinpetclinic.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.*;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity(name = "types")
 public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
