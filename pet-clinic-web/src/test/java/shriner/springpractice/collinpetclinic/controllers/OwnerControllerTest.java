de2a1b9a5a5890c7086cce01c1529ea9a9c95aaf
Collin Shriner <collinshriner9@gmail.com>
2023-06-23 12:47:15 -0400

    added owner record. close #51

--- pet-clinic-web/src/test/java/shriner/springpractice/collinpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/shriner/springpractice/collinpetclinic/controllers/OwnerControllerTest.java
@@ -16,8 +16,9 @@ import shriner.springpractice.collinpetclinic.services.OwnerService;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -63,5 +64,15 @@ class OwnerControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
     }
 }
